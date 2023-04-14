f4d2e2b3ee1d08dcfe1311c21125dd1c5d4138f8
BorisSoaked <borisrunfast@gmail.com>
2023-04-14 16:55:48 +0200

    chess grid made

--- src/Chess/Chess.jsx
+++ src/Chess/Chess.jsx
@@ -1,12 +1,21 @@
 import React from "react"
+import p from './piecesData'
+import './Pieces.css'
+
 
 
 function Chess() {
-    const ChessBoard = new Array(64).fill(<div className="chess-pallet">s</div>)
+    const grid = new Array(64)
+    console.log(p.p)
+
+    for (let i = 8; i < 16; i++) {
+        grid.push(<div key={i} style={{backgroundImage: `url(${p.p})`}}></div>)
+    }
+
 
     return ( 
         <div className="chess-board">
-            {ChessBoard}
+            {grid}
         </div>
      )
 }
