fa4bddf222172c01aee66e8ebb43275470862479
BorisSoaked <borisrunfast@gmail.com>
2023-04-21 18:50:47 +0200

    rewriting everything

--- src/Chess/Chess.jsx
+++ src/Chess/Chess.jsx
@@ -1,70 +1,39 @@
 import React from "react"
-import pieceUrl from './piecesData'
 import './Pieces.css'
-import Piece from "./Piece"
+
 
 
 
 function Chess() {
+
     // Chess Board Declarations:
-    const x = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-    const y = ['8', '7', '6', '5', '4', '3', '2', '1']
     let board = []
-
     // Builds the board it self:
-    for (let i = 0; i < x.length; i++) {
-        for (let j = 0; j < y.length; j++) {
-            board.push(<div className="tile" key={j + i}></div>)
-        } 
-    }
-
-    // Places balack and whithe Pieces
-    board = board.map ( (item, i) => {
-        if (i === 0) {
-            item = <Piece key={i} pieceUrl={pieceUrl.r} />
-            board[i + 1] = <Piece key={i + 1} pieceUrl={pieceUrl.n} />
-            board[i + 2] = <Piece key={i + 2} pieceUrl={pieceUrl.b} />
-            board[i + 3] = <Piece key={i + 4} pieceUrl={pieceUrl.q} />
-            board[i + 4] = <Piece key={i + 3} pieceUrl={pieceUrl.k} />
-            board[i + 5] = <Piece key={i + 5} pieceUrl={pieceUrl.b} />
-            board[i + 6] = <Piece key={i + 6} pieceUrl={pieceUrl.n} />
-            board[i + 7] = <Piece key={i + 7} pieceUrl={pieceUrl.r} />
-        } else if (i > 7 && i < 16) {
-            item = <Piece key={i} pieceUrl={pieceUrl.p} />
-        } else if(i > 47 && i < 56) {
-            item = <Piece key={i} pieceUrl={pieceUrl.P} />
-        } else if (i === 56) {
-            item = <Piece key={i} pieceUrl={pieceUrl.R} />
-            board[i + 1] = <Piece key={i + 1} pieceUrl={pieceUrl.N} />
-            board[i + 2] = <Piece key={i + 2} pieceUrl={pieceUrl.B} />
-            board[i + 3] = <Piece key={i + 4} pieceUrl={pieceUrl.Q} />
-            board[i + 4] = <Piece key={i + 3} pieceUrl={pieceUrl.K} />
-            board[i + 5] = <Piece key={i + 5} pieceUrl={pieceUrl.B} />
-            board[i + 6] = <Piece key={i + 6} pieceUrl={pieceUrl.N} />
-            board[i + 7] = <Piece key={i + 7} pieceUrl={pieceUrl.R} />
-        }
-        return item
-    })
-
-
-    function grabPiece(e) {
-        const elem = e.target
-        if(elem.classList.contains('piece')) {
-           const x = elem.clientX * .5
-           const y = elem.clientY * .5
-
-           elem.style.width = '50px'
-           elem.style.position = 'absolute'
-           elem.style.top = `${x}px`
-           elem.style.left = `${y}px`
+    for (let i = 0; i < 8; i++) {
+        board.push([])
+        for(let j = 0; j < 8; j++) {
+            board[i].push('')
         }
     }
 
 
+    for(let i = 0; i < 8; i++) {
+        board[2][i] = 'wk'
+    }
+    console.log(board)
+
+    for (let x = 0; x < 8; x++) {
+        for(let y = 0; y < 8; y++) {
+            board[x][y] ?
+            board[x][y] = <div key={`${x}${y}`} className="piece wp"></div> :
+            null
+        }
+    }
 
     return ( 
-        <div className="chess-board" onMouseDown={(e) => grabPiece(e)}>
+        <div className="chess-board" >
             {board}
+            {console.log(board)}
         </div>
      )
 }
