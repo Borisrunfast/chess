b04a1770b92bfff2dbd0006cb4e1b8dca4a224ad
BorisSoaked <borisrunfast@gmail.com>
2023-04-24 20:54:30 +0200

    update board, drag n drop

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { useReducer } from 'react'
 import Chess from './Chess/Chess'
+import context from './Context/Context'
+import { reducer } from './Reducer/reducer'
 import './App.css'
 
 function App() {
-
+  const [chessState, dispach] = useReducer(reducer, )
 
   return (
     <div className="App">
-      <Chess />
-      
+      <context.Provider value={{}}>
+        <Chess />
+      </context.Provider>
     </div>
   )
 }
---
+++ src/Reducer/reducer.jsx
@@ -0,0 +1,8 @@
+function reducer(state, action) {
+    switch (action.type) {
+        
+    }
+     
+}
+
+export {reducer}
