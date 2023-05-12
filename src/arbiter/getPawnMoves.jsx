aceb879ca322da0f824d3f740cf18ecb830ebacc
BorisSoaked <borisrunfast@gmail.com>
2023-05-12 16:50:35 +0200

    pawn side attack

--- src/arbiter/getPawnMoves.jsx
+++ src/arbiter/getPawnMoves.jsx
@@ -11,8 +11,8 @@ export function getPawnMoves({position, piece, tile}) {
     ]
 
     const attackingDirection = [
-        piece[0] === 'w'? [1, -1] : [1, 1],
-        piece[0] === 'w'? [-1, -1] : [-1, 1]
+        piece[0] === 'w'? [-1, -1] : [1, 1],
+        piece[0] === 'w'? [-1, 1] : [1, -1]
     ]
     
 
@@ -26,29 +26,30 @@ export function getPawnMoves({position, piece, tile}) {
             const attackXL = tile.x + attackingDirection[0][0]
             const attackXR = tile.x + attackingDirection[1][0]
             
-            const attackY = tile.y + attackingDirection[0][1]
+            const attackYL = tile.y + attackingDirection[0][1]
+            const attckYR = tile.y + attackingDirection[1][1]
 
-            console.log(attackXL)
-            console.log(position[attackXL][attackY].props?.piece[0])  
-            console.log(attackXR)          
-            console.log(position[attackXR][attackY].props?.piece[0] === enemy)  
+
+            console.log(position[attackXL][attackYL]?.props?.piece)  
+  
+            console.log(position[attackXR][attckYR]?.props?.piece)  
 
             if (position[x] === undefined || position[x][y] === undefined) {
                 break
             }
-            if(position[attackXL][attackY].props?.piece[0] === enemy || position[attackXR][attackY].props?.piece[0] === enemy) {
-                if(position[attackXL][attackY].props?.piece[0] === enemy) {
-                    moves.push([attackXL, attackY])
+            if(position[attackXL][attackYL]?.props?.piece[0] === enemy || position[attackXR][attckYR]?.props?.piece[0] === enemy) {
+                if(position[attackXL][attackYL]?.props?.piece[0] === enemy) {
+                    moves.push([attackXL, attackYL])
                 }
-                if (position[attackXR][attackY].props?.piece[0] === enemy) {
-                    moves.push([attackXR, attackY])
+                if (position[attackXR][attckYR]?.props?.piece[0] === enemy) {
+                    moves.push([attackXR, attckYR])
                 }
                 break
             }
-            if (position[x][y].props?.piece[0] === enemy){
+            if (position[x][y]?.props?.piece[0] === enemy){
                 break
             }
-            if (position[x][y].props?.piece[0] === us) {
+            if (position[x][y]?.props?.piece[0] === us) {
                 break
             }
             moves.push([x, y])
